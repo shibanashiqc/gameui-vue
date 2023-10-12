d1cecb1f282befd0251c505b60c7d1e02b19d3b9
Shiban Ashiq <shibanashiq743@gmail.com>
2023-10-13 01:04:13 +0530

    xs

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,14 @@
 <template>
   
-  <nav class="backdrop-blur fixed w-full z-23 top-0 left-0 bg-black/50">
+  <nav class="backdrop-blur fixed w-full z-23 top-0 left-0 bg-black/80">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3 lg:p-6">
       <a href="https://flowbite.com/" class="flex items-center">
         <span class="self-center text-2xl font-semibold whitespace-nowrap text-white x-font">ETF</span>
       </a>
       <div class="flex md:order-2 space-x-0.5">
         
-        
-        <a  style="font-size: 19px;/*! margin-left: -2px; */position: relative;right: 5px;height: 41px;" href="#" class="border-dotted border-2 border-white  w-12">
+        <!-- border-dotted border-2 -->
+        <a  style="font-size: 19px;/*! margin-left: -2px; */position: relative;right: 5px;height: 41px;" href="#" class=" border-white  w-12">
           
           <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" style="position: relative; top: 8px; left: 11px;" class="text-center bi bi-twitter-x text-white" viewBox="0 0 16 16">
           <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
@@ -17,7 +17,7 @@
         </a>
        
         
-        <a href="#" class="mx-5 border-dotted border-2 border-white  w-12">
+        <a href="#" class="mx-5  border-white  w-12">
           <svg style="position: relative; top: 8px; left: 11px;" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telegram text-white" viewBox="0 0 16 16">
           <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
         </svg>
